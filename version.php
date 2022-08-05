19a1d14646318b6772a43cf326781e034d657e05
David Herney <davidherney@gmail.com>
2022-08-05 17:26:14 -0500

    Se condiciona el listado de usuarios a que estén o no suspendidos

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2018072302;       // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018072303;       // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017050500;       // Requires this Moodle version.
 $plugin->component = 'report_userapproval'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = ':)';
+$plugin->release = ':-)';
